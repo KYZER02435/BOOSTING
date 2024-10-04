2f82458b786fd0deadb2866cb84ea34b571baa47
KYZER02435 <levinfuexo@gmail.com>
2024-10-04 08:18:41 +0800

    Update boost.py

--- boost.py
+++ boost.py
@@ -1,10 +1,13 @@
 import os
 import shutil
+import subprocess  # To run git pull command
+
 # Define color variables
 red = "\033[1;31m"    # Bold red
 c = "\033[1;96m"      # Cyan (for overview heading)
 r = "\033[0m"         # Reset color
 wh = "\033[1;37m"     # Bold white
+
 def clear_screen():
     os.system('clear')
 
@@ -28,10 +31,15 @@ def clone_and_run(repo_url, script_name):
     if not os.path.exists(repo_name):
         os.system(f'git clone {repo_url}')
 
+    # Perform git pull to update the repo if already cloned
+    try:
+        subprocess.run(['git', 'pull'], cwd=repo_name, check=True)
+        print("Repository updated successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while updating: {e}")
+
     os.chdir(repo_name)
-    
     os.system(f'python {script_name}')
-    
     os.chdir('..')
 
 def main_menu():
@@ -66,7 +74,7 @@ def main_menu():
     elif choice == '7':
         auto_react_comment()
     elif choice == '8':
-    	auto_working_vid()
+        auto_working_vid()
     elif choice == '0':
         reset()
     elif choice == 'E':
